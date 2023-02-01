38231315b5fc0b281741b52a0bf4320a4cb996ca
cheolgyun7 <cjfrbs1256@gmail.com>
2023-02-01 10:52:54 +0900

    '230201useMemo'

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
+import { useMemo, useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
+// import OptimizeTest from './OptimizeTest';
 
 // https://jsonplaceholder.typicode.com/comments
 
@@ -10,30 +11,31 @@ function App() {
 
   const dataId = useRef(0);
 
-  
 
-  const getData = async() => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res)=>res.json());
-    
-    const initData = res.slice(0,20).map((it)=>{
+
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
       return {
-        author : it.email,
-        content : it.body,
-        emotion : Math.floor(Math.random() * 5)+1,
-        created_date : new Date().getTime(),
-        id : dataId.current++
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++
       }
-      
+
     })
 
     setData(initData);
   };
-//app 컴포넌트가 mount되자마자 호출
-//방법: useEffect에 array에 빈배열을 전달하면 콜백함수는 앱 컴포넌트는
-//mount되자마자 호출된다.
-  useEffect(()=>{
+  //app 컴포넌트가 mount되자마자 호출
+  //방법: useEffect에 array에 빈배열을 전달하면 콜백함수는 앱 컴포넌트는
+  //mount되자마자 호출된다.
+  useEffect(() => {
     getData();
-  },[])
+  }, [])
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -42,29 +44,44 @@ function App() {
       content,
       emotion,
       created_date,
-      id : dataId.current,
+      id: dataId.current,
     }
     dataId.current += 1;
     setData([newItem, ...data]);
   };
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다`)
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   }
 
-  const onEdit = (targetId,newContent) =>{
+  const onEdit = (targetId, newContent) => {
     setData(
       data.map((it) =>
-       it.id === targetId ? {...it, content:newContent}: it
-       )
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
     );
   };
 
+  const getDiaryAnalysis = useMemo(
+    () => {
+      const goodCount = data.filter((it) => it.emotion >= 3).length;
+      const badCount = data.length - goodCount;
+      const goodRatio = (goodCount / data.length) * 100;
+      return { goodCount, badCount, goodRatio };
+
+    }, [data.length]
+  );
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
-      <DiaryEditor onCreate={onCreate}/>
+      <DiaryEditor onCreate={onCreate} />
+      <div>전체일기: {data.length}</div>
+      <div>기분 좋은 일기 개수: {goodCount}</div>
+      <div>기분 나쁜 일기 개수: {badCount}</div>
+      <div>기분 좋은 일기 비율: {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
