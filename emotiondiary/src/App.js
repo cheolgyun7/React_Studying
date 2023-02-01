f4837193775c9adef803244796030470fe18b619
cheolgyun7 <cjfrbs1256@gmail.com>
2023-02-01 15:57:40 +0900

    emotiondiary base

--- emotiondiary/src/App.js
+++ emotiondiary/src/App.js
@@ -6,20 +6,45 @@ import Edit from './pages/Edit';
 import New from './pages/New';
 import Diary from './pages/Diary';
 
-import RouteTest from './components/RouteTest';
+//Components
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader headText={"App"}
+          leftChild={<MyButton text={"왼쪽 버튼"}
+            onClick={() => alert("왼쪽클릭입니다")} />
+          }
+          rightChild={<MyButton text={"오른쪽 버튼"}
+          onClick={() => alert("오른쪽클릭입니다")} />
+        }
+        />
         <h2>app.js</h2>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+        />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/new' element={<New />} />
           <Route path='/Edit' element={<Edit />} />
-          <Route path='/Diary' element={<Diary />} />
+          <Route path='/Diary/id' element={<Diary />} />
         </Routes>
-        <RouteTest/>
+
       </div>
     </BrowserRouter>
   );
--- emotiondiary/src/pages/Edit.js
+++ emotiondiary/src/pages/Edit.js
@@ -1,8 +1,35 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit =() => {
+
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get("id");
+    console.log("id : ", id);
+
+    const mode = searchParams.get("mode");
+    console.log("mode : ",mode);
+
     return (
         <div>
             <h3>Edit</h3>
             <p>이곳은 Edit 입니다</p>
+            <button onClick={()=> setSearchParams({who: "KIM"})}>
+                QS바꾸기
+            </button>
+
+            <button 
+            onClick={()=> {
+                navigate("/home");
+            }}>
+                Home으로 가기
+            </button>
+            <button onClick={()=>{
+                navigate(-1);
+            }}>
+                뒤로가기
+            </button>
         </div>
     );
 };
