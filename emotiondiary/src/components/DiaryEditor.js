84e75b1469bcb9448f5f0398dd04e7f11a1ed25c
cheolgyun7 <cjfrbs1256@gmail.com>
2023-02-28 18:35:36 +0900

    '230228'

--- emotiondiary/src/components/DiaryEditor.js
+++ emotiondiary/src/components/DiaryEditor.js
@@ -6,35 +6,6 @@ import { getStringDate } from "../util/date";
 import MyButton from "./MyButton";
 import MyHeader from "./MyHeader";
 
-//감정에 대한 데이터 배열이 필요함
-const emotionList = [
-    {
-        emotion_id: 1,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-        emotion_descript: "완전 좋음"
-    },
-    {
-        emotion_id: 2,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-        emotion_descript: "좋음"
-    },
-    {
-        emotion_id: 3,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-        emotion_descript: "그럭저럭"
-    },
-    {
-        emotion_id: 4,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-        emotion_descript: "나쁨"
-    },
-    {
-        emotion_id: 5,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-        emotion_descript: "끔찍함"
-    },
-]
-
 
 const DiaryEditor = ({ isEdit, originData }) => {
 
